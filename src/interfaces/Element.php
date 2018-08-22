65721823ec1ac67533c5dc9701fa73bc3969429f
Heiko Griga <hgriga@teilehaber.de>
2018-08-22 12:19:18 +0200

    test

---
+++ src/interfaces/Element.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Desc
+ *
+ * @author Heiko Griga <h.griga@teilehaber.de>
+ * @date   22.08.2018, 12:04
+ */
+
+namespace interfaces;
+
+/**
+ * Class Element
+ * @package interfaces
+ */
+interface Element
+{
+    
+    public function setElement (int $idElement): void;
+}
