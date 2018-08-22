58a49e2c5751315dd694be441be9eefa60a821f4
Heiko Griga <hgriga@teilehaber.de>
2018-08-22 12:34:26 +0200

    test

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Desc
+ *
+ * @author Heiko Griga <h.griga@teilehaber.de>
+ * @date   22.08.2018, 12:28
+ */
+
+require_once __DIR__ . './vendor/autoload.php'; // Autoload files using Composer autoload
+
+
+echo Siscrad\Tables\Consts\Consts::ELEMENT_TABLE;
